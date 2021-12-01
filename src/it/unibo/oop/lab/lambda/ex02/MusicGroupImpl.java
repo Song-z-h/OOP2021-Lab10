fbb62023ce73a0e8a4bfc4290241fd6f64586a8c
Song Zhaohui <zhaohui.song@studio.unibo.it>
2021-12-01 11:07:13 +0100

    impl ex02 toComplete

--- src/it/unibo/oop/lab/lambda/ex02/MusicGroupImpl.java
+++ src/it/unibo/oop/lab/lambda/ex02/MusicGroupImpl.java
@@ -1,11 +1,15 @@
 package it.unibo.oop.lab.lambda.ex02;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Optional;
 import java.util.OptionalDouble;
 import java.util.Set;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -13,111 +17,117 @@ import java.util.stream.Stream;
  */
 public final class MusicGroupImpl implements MusicGroup {
 
-    private final Map<String, Integer> albums = new HashMap<>();
-    private final Set<Song> songs = new HashSet<>();
+	private final Map<String, Integer> albums = new HashMap<>();
+	private final Set<Song> songs = new HashSet<>();
 
-    @Override
-    public void addAlbum(final String albumName, final int year) {
-        this.albums.put(albumName, year);
-    }
+	@Override
+	public void addAlbum(final String albumName, final int year) {
+		this.albums.put(albumName, year);
+	}
 
-    @Override
-    public void addSong(final String songName, final Optional<String> albumName, final double duration) {
-        if (albumName.isPresent() && !this.albums.containsKey(albumName.get())) {
-            throw new IllegalArgumentException("invalid album name");
-        }
-        this.songs.add(new MusicGroupImpl.Song(songName, albumName, duration));
-    }
+	@Override
+	public void addSong(final String songName, final Optional<String> albumName, final double duration) {
+		if (albumName.isPresent() && !this.albums.containsKey(albumName.get())) {
+			throw new IllegalArgumentException("invalid album name");
+		}
+		this.songs.add(new MusicGroupImpl.Song(songName, albumName, duration));
+	}
 
-    @Override
-    public Stream<String> orderedSongNames() {
-        return null;
-    }
+	@Override
+	public Stream<String> orderedSongNames() {
+		return songs.stream().map(x -> x.getSongName()).sorted();
+	}
 
-    @Override
-    public Stream<String> albumNames() {
-        return null;
-    }
+	@Override
+	public Stream<String> albumNames() {
+		return albums.keySet().stream();
+	}
 
-    @Override
-    public Stream<String> albumInYear(final int year) {
-        return null;
-    }
+	@Override
+	public Stream<String> albumInYear(final int year) {
+		return albums.entrySet().stream().filter(alb -> alb.getValue().equals(year)).map(x -> x.getKey());
+	}
 
-    @Override
-    public int countSongs(final String albumName) {
-        return -1;
-    }
+	@Override
+	public int countSongs(final String albumName) {
+		// albumName can be absent since is optional, so check it with isPresent before
+		// working on it.
+		return (int) songs.stream().filter(s -> s.getAlbumName().isPresent())
+				.filter(s -> s.getSongName().equals(albumName)).count();
+	}
 
-    @Override
-    public int countSongsInNoAlbum() {
-        return -1;
-    }
+	@Override
+	public int countSongsInNoAlbum() {
+		return (int) songs.stream().filter(s -> s.getAlbumName().isEmpty()).count();
+	}
 
-    @Override
-    public OptionalDouble averageDurationOfSongs(final String albumName) {
-        return null;
-    }
+	@Override
+	public OptionalDouble averageDurationOfSongs(final String albumName) {
+		return songs.stream().filter(s -> s.getAlbumName().isPresent())
+				.filter(s -> s.getAlbumName().equals(Optional.ofNullable(albumName))).mapToDouble(x -> x.getDuration())
+				.average();
+	}
 
-    @Override
-    public Optional<String> longestSong() {
-        return null;
-    }
+	@Override
+	public Optional<String> longestSong() {
+		return songs.stream().collect(Collectors.maxBy(Comparator.comparingDouble(x -> x.getDuration())))
+				.map(Song::getAlbumName).get();
+	}
 
-    @Override
-    public Optional<String> longestAlbum() {
-        return null;
-    }
+	@Override
+	public Optional<String> longestAlbum() {
+		return null;
+	}
 
-    private static final class Song {
+	private static final class Song {
 
-        private final String songName;
-        private final Optional<String> albumName;
-        private final double duration;
-        private int hash;
+		private final String songName;
+		private final Optional<String> albumName;
+		private final double duration;
+		private int hash;
 
-        Song(final String name, final Optional<String> album, final double len) {
-            super();
-            this.songName = name;
-            this.albumName = album;
-            this.duration = len;
-        }
+		Song(final String name, final Optional<String> album, final double len) {
+			super();
+			this.songName = name;
+			this.albumName = album;
+			this.duration = len;
+		}
 
-        public String getSongName() {
-            return songName;
-        }
+		public String getSongName() {
+			return songName;
+		}
 
-        public Optional<String> getAlbumName() {
-            return albumName;
-        }
+		public Optional<String> getAlbumName() {
+			return albumName;
+		}
 
-        public double getDuration() {
-            return duration;
-        }
+		public double getDuration() {
+			return duration;
+		}
 
-        @Override
-        public int hashCode() {
-            if (hash == 0) {
-                hash = songName.hashCode() ^ albumName.hashCode() ^ Double.hashCode(duration);
-            }
-            return hash;
-        }
+		@Override
+		public int hashCode() {
+			if (hash == 0) {
+				hash = songName.hashCode() ^ albumName.hashCode() ^ Double.hashCode(duration);
+			}
+			return hash;
+		}
 
-        @Override
-        public boolean equals(final Object obj) {
-            if (obj instanceof Song) {
-                final Song other = (Song) obj;
-                return albumName.equals(other.albumName) && songName.equals(other.songName)
-                        && duration == other.duration;
-            }
-            return false;
-        }
+		@Override
+		public boolean equals(final Object obj) {
+			if (obj instanceof Song) {
+				final Song other = (Song) obj;
+				return albumName.equals(other.albumName) && songName.equals(other.songName)
+						&& duration == other.duration;
+			}
+			return false;
+		}
 
-        @Override
-        public String toString() {
-            return "Song [songName=" + songName + ", albumName=" + albumName + ", duration=" + duration + "]";
-        }
+		@Override
+		public String toString() {
+			return "Song [songName=" + songName + ", albumName=" + albumName + ", duration=" + duration + "]";
+		}
 
-    }
+	}
 
 }
